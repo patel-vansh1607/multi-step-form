fc351257672bee328eb35c91a8bc669d35c57a27
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-19 21:13:57 +0300

    adding the steps

--- src/components/MultiForm.js
+++ src/components/MultiForm.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import '../styles/MultiForm.css';
 import { FaUser, FaPhone, FaBuilding, FaAlignLeft } from 'react-icons/fa';
-
+const steps = [
+    { label: 'Personal profile', icon: <FaUser /> },
+    { label: 'Contacts', icon: <FaPhone /> },
+    { label: 'Company', icon: <FaBuilding /> },
+    { label: 'Description', icon: <FaAlignLeft /> }
+  ];
 const MultiForm = () => {
     return(
         <div className="main-div">
