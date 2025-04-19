df4c0dc3c1af11b0cd3a7c49746e902ebfda008e
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-19 21:13:42 +0300

    importing css , react and icons

--- src/components/MultiForm.js
+++ src/components/MultiForm.js
@@ -1,3 +1,7 @@
+import React, { useState } from 'react';
+import '../styles/MultiForm.css';
+import { FaUser, FaPhone, FaBuilding, FaAlignLeft } from 'react-icons/fa';
+
 const MultiForm = () => {
     return(
         <div className="main-div">
