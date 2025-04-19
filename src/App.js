318e20f7688f74060040eb8a238f75728168f142
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-19 10:53:45 +0300

    settting up folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MultiStepForm from './components/MultiForm';
+
+function App() {
+  return (
+    <div className="App">
+      <MultiStepForm />
+    </div>
+  );
+}
+
+export default App;
